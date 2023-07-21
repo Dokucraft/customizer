fe7add4ff85b9c93c69df303be2c691604e22803
Even Torset <eventorset@gmail.com>
2023-07-21 21:31:49 +0200

    Fixed the vanilla leaves models for Dark and High.

--- leaves_model/configure.js
+++ leaves_model/configure.js
@@ -1,18 +1,27 @@
 switch (Config.leaves_model) {
-  case 'vanilla':
-    const folderMap = {
-      light: 'light_dwarven',
-      dwarven: 'light_dwarven',
-      high: 'high_dark',
-      dark: 'high_dark',
+  case 'vanilla': {
+    if (Customizer.version <= 2) {
+      const folderMap = {
+        light: 'light_dwarven',
+        dwarven: 'light_dwarven',
+        high: 'high_dark',
+        dark: 'high_dark',
+      }
+      await Promise.all([
+        Customizer.add('leaves_model/vanilla/blockstates', 'assets/minecraft/blockstates'),
+        Customizer.add(`leaves_model/vanilla/models/${folderMap[Pack.name]}`, 'assets/minecraft/models/block/leaves/vanilla')
+      ])
+    } else {
+      await Promise.all([
+        Customizer.add('leaves_model/vanilla/blockstates', 'assets/minecraft/blockstates'),
+        Customizer.add(`leaves_model/vanilla/models/light_dwarven`, 'assets/minecraft/models/block/leaves/vanilla')
+      ])
     }
-    await Promise.all([
-      Customizer.add('leaves_model/vanilla/blockstates', 'assets/minecraft/blockstates'),
-      Customizer.add(`leaves_model/vanilla/models/${folderMap[Pack.name]}`, 'assets/minecraft/models/block/leaves/vanilla')
-    ])
     break
-  case 'bushy':
+  }
+  case 'bushy': {
     await Customizer.add('leaves_model/bushy_leaves', 'assets/minecraft')
 		Customizer.delete('assets/minecraft/optifine/colormap/custom/birch.properties')
     break
+  }
 }
