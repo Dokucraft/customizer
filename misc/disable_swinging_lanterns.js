14e824db660ffbdd1f2efb8ba730b677309bffcd
Even Torset <eventorset@gmail.com>
2023-11-24 20:03:00 +0100

    Fixed disabling the swinging lanterns in version 5+.

--- misc/disable_swinging_lanterns.js
+++ misc/disable_swinging_lanterns.js
@@ -1,1 +1,6 @@
-await Customizer.shaders.disable('assets/minecraft/shaders/config.txt', 'ENABLE_LANTERN_SWAY')
+if (Customizer.version >= 5) {
+  // Version 5 (1.20.3) renamed this shader option from "sway" to "swing"
+  await Customizer.shaders.disable('assets/minecraft/shaders/config.txt', 'ENABLE_LANTERN_SWING')
+} else {
+  await Customizer.shaders.disable('assets/minecraft/shaders/config.txt', 'ENABLE_LANTERN_SWAY')
+}
