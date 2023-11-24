cee38480cee86837dfa4619229307d5b06002be6
Even Torset <eventorset@gmail.com>
2023-11-24 20:04:06 +0100

    Added flavor-specific textures for the dense grass effect option.

--- grass_effect/grass_effect.js
+++ grass_effect/grass_effect.js
@@ -5,5 +5,8 @@ const grassTypes = {
 
 await Promise.all([
   Customizer.shaders.setScalar('assets/minecraft/shaders/config.txt', 'GRASS_TYPE', grassTypes[Config.grass_effect]),
-  Customizer.add('grass_effect/' + Config.grass_effect, 'assets/minecraft')
+  Customizer.add('grass_effect/' + Config.grass_effect, 'assets/minecraft'),
+  Config.grass_effect === 'dense' ?
+    Customizer.add(`grass_effect/dense_flavor_specific/grass_block_top_${Pack.name}.png`, 'assets/minecraft/textures/block/grass_block_top.png')
+  : 0
 ])
