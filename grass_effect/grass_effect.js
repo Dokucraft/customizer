398fa50fbf204bc0d7218c3b3b34f1fee9616d77
Even Torset <eventorset@gmail.com>
2024-01-05 05:21:30 +0100

    Fixed dense grass sometimes breaking everything.

--- grass_effect/grass_effect.js
+++ grass_effect/grass_effect.js
@@ -3,10 +3,15 @@ const grassTypes = {
   dense: 2
 }
 
+if (!(Config.grass_effect in grassTypes)) {
+  throw 'Invalid grass type'
+}
+
 await Promise.all([
   Customizer.shaders.setScalar('assets/minecraft/shaders/config.txt', 'GRASS_TYPE', grassTypes[Config.grass_effect]),
-  Customizer.add('grass_effect/' + Config.grass_effect, 'assets/minecraft'),
-  Config.grass_effect === 'dense' ?
-    Customizer.add(`grass_effect/dense_flavor_specific/grass_block_top_${Pack.name}.png`, 'assets/minecraft/textures/block/grass_block_top.png')
-  : 0
+  Customizer.add('grass_effect/' + Config.grass_effect, 'assets/minecraft')
 ])
+
+if (Config.grass_effect === 'dense') {
+  await Customizer.add(`grass_effect/dense_flavor_specific/grass_block_top_${Pack.name}.png`, 'assets/minecraft/textures/block/grass_block_top.png')
+}
