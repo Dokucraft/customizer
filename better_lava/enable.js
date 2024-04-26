996ef3a860b4870fbde2efbf71e62122e86471e4
Even Torset <eventorset@gmail.com>
2024-04-26 23:00:53 +0200

    Automatically add GameTime to solid shader for Better Lava. This makes the GameTime uniform optional in the solid shader JSON, which means it can be removed from the base packs without breaking the Better Lava option. This gets rid of the annoying warning about GameTime not being used in the shader when Better Lava isn't enabled.

--- better_lava/enable.js
+++ better_lava/enable.js
@@ -1,5 +1,17 @@
 Customizer.delete('assets/minecraft/textures/block/lava_still.png.mcmeta')
 await Promise.all([
   Customizer.shaders.enable('assets/minecraft/shaders/config.txt', 'ENABLE_BETTER_LAVA'),
-  Customizer.add('better_lava/lava_still.png', 'assets/minecraft/textures/block/lava_still.png')
+  Customizer.add('better_lava/lava_still.png', 'assets/minecraft/textures/block/lava_still.png'),
+  Customizer.readFile('assets/minecraft/shaders/core/rendertype_solid.json').then(async content => {
+    const json = JSON.parse(content)
+    if (!json.uniforms.some(e => e.name === 'GameTime')) {
+      json.uniforms.push({
+        name: 'GameTime',
+        type: 'float',
+        count: 1,
+        values: [ 0.0 ]
+      })
+      await Customizer.writeFile('assets/minecraft/shaders/core/rendertype_solid.json', JSON.stringify(json))
+    }
+  })
 ])
