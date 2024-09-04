a4f4adabaff65a1d886963a92cf93929139c0f4f
Even Torset <eventorset@gmail.com>
2024-09-04 22:14:38 +0200

    Fixed the sketch colors breaking everything if they were default.

--- menu_background/sketch.js
+++ menu_background/sketch.js
@@ -1,7 +1,13 @@
 await Customizer.shaders.enable('assets/minecraft/shaders/program/blur.fsh', 'RADIUS_OVERRIDE')
 await Customizer.shaders.setScalar('assets/minecraft/shaders/program/menu-bg.fsh', 'MENU_BACKGROUND', 2)
-await Customizer.shaders.setVec3('assets/minecraft/shaders/program/menu-bg.fsh', 'SKETCH_PAPER_COLOR', Config.menu_background_sketch_paper_color)
-await Customizer.shaders.setVec3('assets/minecraft/shaders/program/menu-bg.fsh', 'SKETCH_INK_COLOR', Config.menu_background_sketch_ink_color)
+
+if ('menu_background_sketch_paper_color' in Config) {
+  await Customizer.shaders.setVec3('assets/minecraft/shaders/program/menu-bg.fsh', 'SKETCH_PAPER_COLOR', Config.menu_background_sketch_paper_color)
+}
+
+if ('menu_background_sketch_ink_color' in Config) {
+  await Customizer.shaders.setVec3('assets/minecraft/shaders/program/menu-bg.fsh', 'SKETCH_INK_COLOR', Config.menu_background_sketch_ink_color)
+}
 
 if (!Config.menu_background_sketch_paper_texture) {
   await Customizer.shaders.disable('assets/minecraft/shaders/program/menu-bg.fsh', 'SKETCH_PAPER_TEXTURE')
