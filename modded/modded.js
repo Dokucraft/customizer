4f67b0039a7cfdd548b86f9fa637aded1227da3e
SyndiShanX <brianmatthew.1026@gmail.com>
2024-12-31 01:27:59 -0500

    Added Pale Oak Leaves and Fixed Modded Script

--- modded/modded.js
+++ modded/modded.js
@@ -3,17 +3,18 @@ await Promise.all([
 	Customizer.add(`modded/redstone_lamp_on.json`, 'assets/minecraft/models/block/redstone_lamp_on.json'),
 	
 	Customizer.add(`modded/block`, 'assets/minecraft/textures/block'),
-	Customizer.add(`modded/item`, 'assets/minecraft/textures/item')
+	Customizer.add(`modded/item`, 'assets/minecraft/textures/item'),
 	
-	Customizer.delete('assets/minecraft/models/block/froglights')
-	Customizer.delete('assets/minecraft/models/block/blue_ice.json')
-	Customizer.delete('assets/minecraft/models/block/diamond_block.json')
-	Customizer.delete('assets/minecraft/models/block/diamond_ore.json')
-	Customizer.delete('assets/minecraft/models/block/emerald_block.json')
-	Customizer.delete('assets/minecraft/models/block/emerald_ore.json')
-	Customizer.delete('assets/minecraft/models/block/packed_ice.json')
-	
-	Customizer.delete('assets/minecraft/blockstates/ochre_froglight.json')
-	Customizer.delete('assets/minecraft/blockstates/pearlescent_froglight.json')
-	Customizer.delete('assets/minecraft/blockstates/verdant_froglight.json')
+	Customizer.delete(
+		'assets/minecraft/models/block/froglights',
+		'assets/minecraft/models/block/blue_ice.json',
+		'assets/minecraft/models/block/diamond_block.json',
+		'assets/minecraft/models/block/diamond_ore.json',
+		'assets/minecraft/models/block/emerald_block.json',
+		'assets/minecraft/models/block/emerald_ore.json',
+		'assets/minecraft/models/block/packed_ice.json',
+		'assets/minecraft/blockstates/ochre_froglight.json',
+		'assets/minecraft/blockstates/pearlescent_froglight.json',
+		'assets/minecraft/blockstates/verdant_froglight.json',
+  )
 ])
